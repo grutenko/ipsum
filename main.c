658edf4263bb58ae1d655a1c276d4c6460f09259
alexey <alexfedor932@gmail.com>
2025-05-18 21:04:49 +0300

    rename main module

---
+++ main.c
@@ -0,0 +1,6 @@
+
+// This project is currently a **concept and design prototype**.
+// Commands, interfaces, and internal structures are being explored and
+// iterated. Implementation is partial or not yet started.
+
+int main(int argc, char **argv) { return 0; }
